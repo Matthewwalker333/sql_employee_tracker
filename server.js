fe9aa9f81e33949b123c24c5131e57917a4c31e5
Matthew Walker <Matthewwalkermw64@gmail.com>
2021-12-15 20:24:59 -0800

    password bug fixed

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const connection = mysql.createConnection({
     user: 'root',
 
     // Your password
-    password: 'PlacePassWordHere',
+    password: 'Midnight64',
     database: 'employeesDB'
 });
 
